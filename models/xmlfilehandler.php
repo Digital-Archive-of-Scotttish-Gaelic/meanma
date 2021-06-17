ff831589750c4330663f2c7ea3c2a2c00db97f95
steviebarrett <mail@steviebarrett.com>
2021-06-17 14:33:48 +0100

    Fixed issue with display of internal tags in slip context

--- models/xmlfilehandler.php
+++ models/xmlfilehandler.php
@@ -112,109 +112,48 @@ XPATH;
 		return $context;
 	}
 
-  public function getContext_old($id, $preScope = 20, $postScope = 20, $normalisePunc = true, $tagCollocates = false, $tagContext = false) {
-  	$this->_preScope = $preScope;
-  	$this->_postScope = $postScope;
-    $context = array();
-    $context["id"] = $id;   //now takes place of ["headwordId"] as well as ["id"]
-    $context["filename"] = $this->getFilename();
-	 // echo "<br>" . $this->_filename . " : {$id}";    // handy for debugging XML issues SB
-	  // run xpath on p or lg or h or list element - possibly revert after MSS project
-	  $xpath = <<<XPATH
-			//dasg:w[@id='{$id}']/ancestor::*[name()='p' or name()='lg' or name()='h' or name()='list']
-XPATH;
-    $subXML = $this->_xml->xpath($xpath)[0];
-    $subXML = new \SimpleXMLElement($subXML->asXML());
-    $xpath = <<<XPATH
-			//w[@id='{$id}']/preceding::*[(name()='w' and not(descendant::w)) or name()='pc' or name()='o']
-XPATH;
-    //for future: how do we add linebreaks for verse??
-    $words = $subXML->xpath($xpath);
-    /* preContext processing */
-    $context["pre"] = array("output"=>"");
-    if ($preScope) {
-      $pre = array_slice($words, -$preScope);
-      //check if we're one token away from the start of the document
-      $nextIndex = $preScope + 1;
-      $limitCheck = array_slice($words, -$nextIndex);
-      if (count($limitCheck) != count($pre)+1) {
-        $context["prelimit"] = count($pre);
-      }
-      if ($normalisePunc) {
-        $context["pre"] = $this->_normalisePunctuation($pre, $tagCollocates, $tagContext, $section = "pre");
-      } else {
-        $context["pre"]["output"] = implode(' ', $pre);
-	    }
-    }
-    /* -- */
-    $xpath = "//dasg:w[@id='{$id}']";
-    $word = $this->_xml->xpath($xpath);
-	  $wordString = functions::cleanForm($word[0]);   //strips tags and whitespace
-    $context["word"] = ($tagCollocates || $tagContext)
-	    ? '<div style="display:inline; margin-left:4px;"><mark>' . $wordString . '</mark></div>'
-      : $wordString;
-
-	  $xpath = "//w[@id='{$id}']/following::*[not(name()='s') and not(name()='p') and not(name()='note')]";
-	  $words = $subXML->xpath($xpath);
-    /* postContext processing */
-    $context["post"] = array("output"=>"");
-    if ($postScope) {
-      $post = array_slice($words,0, $postScope);
-      //check if we're one token away from the end of the document
-      $nextIndex = $postScope + 1;
-      $limitCheck = array_slice($words, 0, $nextIndex);
-      if (count($limitCheck) != count($post)+1) {
-        $context["postlimit"] = count($post);
-      }
-      if ($normalisePunc) {
-        $context["post"] = $this->_normalisePunctuation($post, $tagCollocates, $tagContext, $section = "post");
-      } else {
-        $context["post"]["output"] = implode(' ', $post);
-      }
-      //check if the scope has reached the end of the document
-      if (count($post) < $postScope) {
-        $context["post"]["limit"] = count($post);
-      }
-    }
-    return $context;
-  }
-
   /**
    * Parses an array of SimpleXML objects and formats the punctuation
-   * @param array $chunk : array of SimpleXML objects
-   * @return array : an array containing output string and flags for start and end joins
+   * @param array $chunk : array of SimpleXML objects (w, pc, or o)
+   * @param bool $tagCollocates : flag to set whether to output with HTML markup for handling collocates
+   * @param bool $tagContext :  flag to set whether the output should be HTML markup with tokens clickable by user to trim context
+   * @param string $section : either pre or post
+   * @return associative array : an array containing output string and flags for start and end joins
+   *   output => the context string, possible with HTML markup
+   *   startJoin => one of possible values : left, right, both, none
+   *   endJoin => one of possible values : left, right, both, none
    */
-  private function _normalisePunctuation (array $chunk, $tagCollocates, $tagContext, $section = null) {
+  private function _normalisePunctuation (array $chunk, $tagCollocates, $tagContext, $section) {
     $output = $startJoin = $endJoin = "";
-    $rightJoin = true;
+    $rightJoin = true;  // should this token join to the next
 		$this->_collocateIds = lemmas::getCollocateIds($this->getFilename());
 		//used to track the position of each token in the pre/post context
-		$position = $section == "pre" ? $this->_preScope : 1;
+		$position = $section == "pre" ? $this->_preScope : 1; // the position of this token in the context
     foreach ($chunk as $i => $element) {
     	// !! $isWord is only used when we need to tag collocates
 	    $isWord = false;
 	    if ($tagCollocates) {
-		    $isWord = ($wordId = $element->attributes()["id"]) ? true : false;
+		    $isWord = ($wordId = $element->attributes()["id"]) ? true : false;  //maybe change to name() of element
 	    }
-      $followingWord = ($i < (count($chunk)-1)) ? $chunk[$i+1] : null;
-      $followingJoin = $followingWord ? $followingWord->attributes()["join"] : "";
+      $followingToken = ($i < (count($chunk)-1)) ? $chunk[$i+1] : null;
+      $followingJoin = $followingToken ? $followingToken->attributes()["join"] : "";
       $attributes = $element->attributes();
-      if ($i == 0) {
+      if ($i == 0) {    //first element in the array
         $startJoin = (string)$attributes["join"];
-      } else if ($i == (count($chunk) -1)) {
+      } else if ($i == (count($chunk) -1)) {    //last element in the array
         $endJoin = (string)$attributes["join"];
       }
 
-      $spacer = ' ';
+      $spacer = ' ';    //default to using simple single space character
 			if ($tagCollocates) {
 				$token = $isWord ? $this->_getCollocateDropdown($element, $wordId) : $element[0];
 				$spacer = '<div style="margin-right:-4px;display:inline;">&thinsp;</div>';
 			} else if ($tagContext) {
-				$verb = $section == "pre" ? "start" : "end";
+				$startOrEnd = $section == "pre" ? "start" : "end";
 				$token = '<a data-toggle="tooltip" data-html="true" class="contextLink ' . $section . '" data-position="' . $position . '"';
-				$token .= ' title="' . $verb . ' context with <em><strong>' . $element[0] . '</strong></em>">' . $element[0] . '</a>';
+				$token .= ' title="' . $startOrEnd . ' context with <em><strong>' . $element[0] . '</strong></em>">' . $element[0] . '</a>';
 			} else {
-				$token = $element[0];
+				$token = functions::cleanForm($element[0]); // ensure display of tags within the element (e.g. <abbr>)
 			}
 			//decrement/increment the position in the context
 			$position = $section == "pre" ? $position - 1 : $position + 1;
