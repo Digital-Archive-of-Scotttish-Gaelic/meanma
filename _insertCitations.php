32486746f6ea54a417242c6ebb264dc9e7e7dc9b
steviebarrett <mail@steviebarrett.com>
2021-08-27 14:56:36 +0100

    Created code to generate citations from slips, create links from the slips to the new citations, create translations from the slips, and create links from the citations to the translations.

--- _insertCitations.php
+++ _insertCitations.php
@@ -4,8 +4,50 @@ namespace models;
 
 require_once 'includes/htmlHeader.php';
 
-$db = new models\database();
+/*
+$db = new database();
 
-$slips = collection::getAllSlipInfo()
+//Get all the slips' data
+$sql = <<<SQL
+	SELECT * FROM slips
+SQL;
+$results = $db->fetch($sql);
+foreach ($results as $slip) {
+
+	//create the citation
+	$sql = <<<SQL
+		INSERT INTO citation (preContextScope, postContextScope) 
+			VALUES ({$slip["preContextScope"]}, {$slip["postContextScope"]})
+SQL;
+	$db->exec($sql);
+
+	//create the links between the slip and the new citation
+	$citationId = $db->getLastInsertId();
+	$sql = <<<SQL
+		INSERT INTO slip_citation (slip_id, citation_id) 
+			VALUES ({$slip["auto_id"]}, $citationId)
+SQL;
+	$db->exec($sql);
+
+	//Translations
+	if ($slip["translation"]) {   //only add a translation entry if the slip has translation content
+
+		//create the translation
+		$sql = <<<SQL
+		INSERT INTO translation (content)
+			VALUES (:translation)
+SQL;
+		$db->exec($sql, array(":translation" => $slip["translation"]));
+
+		//create the link between the citation and the new translation
+		$translationId = $db->getLastInsertId();
+		$sql = <<<SQL
+		INSERT INTO citation_translation (citation_id, translation_id)
+			VALUES ({$citationId}, {$translationId})
+SQL;
+		$db->exec($sql);
+	}
+}
+*/
 
 require_once 'includes/htmlFooter.php';
