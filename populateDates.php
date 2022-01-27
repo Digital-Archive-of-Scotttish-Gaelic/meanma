ac1f7d5ebfd1622480911c6831a6ab5fa5fcc785
steviebarrett <mail@steviebarrett.com>
2022-01-27 12:56:58 +0000

    Added populateDates.php to add parent dates to child texts

---
+++ populateDates.php
@@ -0,0 +1,31 @@
+<?php
+
+use models;
+
+require_once "includes/include.php";
+
+if (!models\users::checkSuperuserAuth()) {
+	die ("not authorised");
+}
+
+$db = new \models\database();
+$sql = <<<SQL
+	SELECT id FROM text ORDER BY id;
+SQL;
+$results = $db->fetch($sql);
+foreach ($results as $result) {
+
+	$textId = $result["id"];
+	$text = new models\corpus_browse($textId, $db);
+
+	if ($text->getDate() == NULL) {
+		$date = $text->getParentText()->getDate();
+		if ($date) {
+			echo "<br>{$date}";
+			$updateSql = <<<SQL
+			UPDATE text SET date = {$date} WHERE id = '{$textId}'
+SQL;
+			$db->exec($updateSql);
+		}
+	}
+}
