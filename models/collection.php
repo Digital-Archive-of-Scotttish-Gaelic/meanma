ba8d1df1e2cdc6ddf236709001932e462bec7227
steviebarrett <mail@steviebarrett.com>
2022-06-08 11:23:30 +0100

    Bug fix for deletePilesForSlip

--- models/collection.php
+++ models/collection.php
@@ -386,7 +386,7 @@ SQL;
 SQL;
 			$db->exec($sql, array(":slipId" => $slipId));
 							// delete sense categories for this slip
-			pilecategories::deleteSensesForSlip($slipId);
+			pilecategories::deletePilesForSlip($slipId);
 							// delete citations for this slip
 			$citationIds = self::getCitationIdsForSlip($slipId, $db);
 			foreach ($citationIds as $cid) {
