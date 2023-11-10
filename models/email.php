dce27233c4f6794b73067909d75088da5fc633ea
steviebarrett <mail@steviebarrett.com>
2023-11-10 12:20:14 +0000

    Using PHPMailer to send emails with AWS SMTP authentication. Needs tidying up.

--- models/email.php
+++ models/email.php
@@ -2,57 +2,80 @@
 
 namespace models;
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 class email
 {
   private $to, $subject, $message, $from;
   private $cc = array();
 
+  private $mail;
+
   public function __construct($to, $subject, $message, $from) {
-    $this->to = $to;
-    $this->subject = $subject;
-    $this->message = $message;
-    $this->from = $from;
+      $this->mail = new PHPMailer;
+
+	  $this->mail->SMTPDebug = 0;
+	  $this->mail->isSMTP();
+	  $this->mail->Host = getenv(SMTPHost);
+	  $this->mail->SMTPAuth = true;
+//Provide username and password
+	  $this->mail->Username = getenv(SMTPUser);
+	  $this->mail->Password = getenv(SMTPPass);
+//If SMTP requires TLS encryption then set it
+	  $this->mail->SMTPSecure = "tls";
+//Set TCP port to connect to
+	  $this->mail->Port = 587;
+
+	  $this->mail->addAddress($to);
+      $this->mail->Subject = $subject;
+      $this->mail->Body = $message;
+      $this->mail->From = $from;
+	  $this->mail->isHTML(true);
   }
 
   public function getTo() {
-    return $this->to;
+    return $this->mail->getToAddresses();
   }
 
   public function getSubject() {
-    return $this->subject;
+    return $this->mail->Subject;
   }
 
   public function getMessage() {
-    return $this->message;
+    return $this->mail->Body;
   }
 
   public function getFrom() {
-    return $this->from;
+    return $this->mail->From;
   }
 
   public function setCc($cc) {
-    $this->cc = $cc;
+    $this->mail->addCC($cc);
   }
 
   public function getCc() {
-    return $this->cc;
+    return $this->mail->getCcAddresses();
   }
 
   public function getCcList() {
-    return implode(",", $this->getCc());
+    return implode(",", $this->mail->getCc());
   }
-
+/*
   public function getHeaders() {
-    $headers[] = "From: " . $this->getFrom();
-    $headers[] = "Reply-To: " . $this->getFrom();
-    if($this->getCc())
-      $headers[] = "CC: " . $this->getCcList();
+    $headers[] = "From: " . $this->mail->getFrom();
+    $headers[] = "Reply-To: " . $this->mail->getFrom();
+    if($this->mail->getCc())
+      $headers[] = "CC: " . $this->mail->getCcList();
     $headers[] = "MIME-Version: 1.0";
     $headers[] = "Content-Type: text/html; charset=ISO-8859-1";
     return implode("\r\n", $headers);
   }
-
+*/
   public function send() {
-    mail($this->getTo(), $this->getSubject(), $this->getMessage(), $this->getHeaders());
+    if (!$this->mail->send()) {
+		echo 'Mail send error : ' . $this->mail->ErrorInfo;
+    } else {
+		echo 'Email sent successfully';
+    }
   }
 }
